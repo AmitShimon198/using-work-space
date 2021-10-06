7ae49c418d301a29e2d030a27a251fee8be9a8e4
amitvolga <amit.shimon@gmail.com>
2021-10-07 01:11:00 +0300

    refactoring

--- src/components/displaySum.tsx
+++ src/components/displaySum.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@reuse-react-components/experimenting.ui.button';
-import { NotFoundPage, NotFoundPageProps } from '@teambit/design.ui.pages.not-found';
-import { useEffect, useState } from 'react';
+import { NotFoundPage } from '@teambit/design.ui.pages.not-found';
+import { useState } from 'react';
 import classes from './displaySum.module.css';
 type DisplaySumProps = {
     title: string;
@@ -19,33 +19,46 @@ const DisplayResults = ({ displayValue, matchPair }: DisplayResultsProps) => (<d
 
 const DisplaySum = ({ title, matchPair, onConfirm, findMatchPair, message, displayValue }: DisplaySumProps) => {
     const [disableButton, setDisableButton] = useState<boolean>()
-    let displayContant;
-    if (matchPair?.length) {
-        displayContant = <DisplayResults displayValue={displayValue} matchPair={matchPair} />
-    } else if (message) {
-        displayContant = <NotFoundPage className={classes.notFound} />
-    } else {
-        displayContant = <h1>{title}</h1>
+    const [numberOfSearch, setNumberOfSearch] = useState<number>(0)
+
+    const renderMainContent = () => {
+        let displayContant;
+        if (matchPair?.length) {
+            displayContant = <DisplayResults displayValue={displayValue} matchPair={matchPair} />;
+        } else if (message) {
+            displayContant = <NotFoundPage className={classes.notFound} />;
+        } else {
+            displayContant = <h1>{title}</h1>;
+        }
+        return displayContant;
     }
+
+    let displayContant = renderMainContent();
+
     const onConfirmButtonHandler = (event: any) => {
         setDisableButton(true)
         onConfirm(event);
         setDisableButton(false)
     }
     const onSearchButtonHandler = (event: any) => {
+        setNumberOfSearch(prev => prev + 1)
         setDisableButton(true)
         findMatchPair(event);
         setDisableButton(false)
 
     }
+    const buttonLabel = !!numberOfSearch ? 'Try again' : 'Find A Match';
     return (
         <div className={classes.container}>
+            {!!numberOfSearch && <span>You have try {numberOfSearch} times,</span>}
             {displayContant}
             <div className={classes.buttonContainer}>
                 <Button className={classes.button} disabled={disableButton} onClick={onConfirmButtonHandler}><span>Confirm</span></Button>
-                <Button className={classes.button} disabled={disableButton} onClick={onSearchButtonHandler}><span>Find A Match</span></Button>
+                <Button className={classes.button} disabled={disableButton} onClick={onSearchButtonHandler}><span>{buttonLabel}</span></Button>
             </div>
         </div>
     )
+
+
 }
 export default DisplaySum;
